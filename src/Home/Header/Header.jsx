26de6f67ff89ba41255465efc94ad6dcbd30f7be
mesameskola <skolamesame82@gmail.com>
2025-11-18 13:59:31 +0400

    create header&footer

--- src/Home/Header/Header.jsx
+++ src/Home/Header/Header.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div className="bg-blue-500 p-4 text-white">
+<h1 className="text-2xl font-bold">Welcome to My Website</h1>
+      </div>
+    </div>
   )
 }
